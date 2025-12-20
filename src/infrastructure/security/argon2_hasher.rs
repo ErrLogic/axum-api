a2d2dedca95d687fe1acc970e374ff0d1e184007
ErrLogic <agik.bika.ristiawan@gmail.com>
2025-12-20 22:02:16 +0700

    fix: updating deps and change some deprecated method

--- src/infrastructure/security/argon2_hasher.rs
+++ src/infrastructure/security/argon2_hasher.rs
@@ -1,8 +1,7 @@
 use argon2::{
-    password_hash::{PasswordHash, PasswordHasher as _, PasswordVerifier, SaltString},
+    password_hash::{phc::PasswordHash, PasswordHasher as _, PasswordVerifier},
     Argon2,
 };
-use argon2::password_hash::rand_core::OsRng;
 
 use crate::application::security::password_hasher::{PasswordHashError, PasswordHasher};
 
@@ -13,18 +12,17 @@ pub struct Argon2PasswordHasher {
 impl Argon2PasswordHasher {
     pub fn new() -> Self {
         Self {
-            argon2: Argon2::default(), // argon2id
+            argon2: Argon2::default(),
         }
     }
 }
 
 impl PasswordHasher for Argon2PasswordHasher {
     fn hash(&self, plain: &str) -> Result<String, PasswordHashError> {
-        let salt = SaltString::generate(&mut OsRng);
 
         let hash = self
             .argon2
-            .hash_password(plain.as_bytes(), &salt)
+            .hash_password(plain.as_bytes())
             .map_err(|_| PasswordHashError::HashFailed)?
             .to_string();
 
